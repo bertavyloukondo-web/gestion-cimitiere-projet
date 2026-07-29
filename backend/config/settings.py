6ad964866e53906ee12daf205bc72f49c0227033
Bertavy <bertavyloukondo@gmail.com>
2026-07-29 19:28:11 +0100

    Correction ALLOWED_HOSTS Render

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = 'django-insecure-%3v*@3m@i$e@)0sx09!*+4@grku+he_0la(a7*q&drayn7@+t9
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    "gestion-cimitiere.onrender.com",
+    "gestion-cimitiere-backend.onrender.com",
     "localhost",
     "127.0.0.1",
 ]
